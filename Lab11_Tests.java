288a7b3fcda97d4db0b2886e449c04aaa30b50a4
Ahmadelsaw <ahmade@gwmail.gwu.edu>
2025-11-03 13:45:18 -0500

    DONE

--- Lab11_Tests.java
+++ Lab11_Tests.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -12,19 +13,41 @@ public class Lab11_Tests {
     */
     @Test
     public void test1() {
+
+        new Lab11_Thread("RESET", 0).setData(new ArrayList<>());
+
         Lab11_Thread threadA = new Lab11_Thread("A1", 100);
         Lab11_Thread threadB = new Lab11_Thread("B1", 100);
 
         threadA.start();
         threadB.start();
 
+        try {
+            threadA.join();
+            threadB.join();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // verify exactly 100 items from each thread
+        ArrayList<String> data = threadA.getData();
+        assertEquals(200, data.size());
+
+        int aCount = 0, bCount = 0;
+        for (String s : data) {
+            if (s.startsWith("A1 ")) {
+            aCount++;}
+            if (s.startsWith("B1 ")) {
+                bCount++;}
+        }
+        assertEquals(100, aCount);
+        assertEquals(100, bCount);
     }
 
-    /*
-        Complete the test case below that checks to see if the shared ArrayList has at least 10 entries after 500ms of system time
-    */
     @Test
     public void test2() {
+
+        new Lab11_Thread("RESET", 0).setData(new ArrayList<>());
 
         Lab11_Thread threadA = new Lab11_Thread("A2", 500);
         Lab11_Thread threadB = new Lab11_Thread("B2", 500);
@@ -37,14 +60,25 @@ public class Lab11_Tests {
             e.printStackTrace();
         }
 
+        // after ~500ms (each loop sleeps 50ms), expect at least ~10 total appends
+        int sizeAfterHalfSecond = threadA.getData().size();
+        assertTrue("Expected at least 10 items after ~500ms", sizeAfterHalfSecond >= 10);
+
+        // join so these long threads don't affect other tests
+        try {
+            threadA.join();
+            threadB.join();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    /*
-        Complete the test case below that checks to see if thread A finishes adding its 10 entries before thread B was allowed to 
-        add anything to the shared ArrayList
-    */
+
     @Test
     public void test3() {
+
+        new Lab11_Thread("RESET", 0).setData(new ArrayList<>());
+
         Lab11_Thread threadA = new Lab11_Thread("A3", 10);
         Lab11_Thread threadB = new Lab11_Thread("B3", 10);
 
@@ -57,5 +91,22 @@ public class Lab11_Tests {
         }
         
         threadB.start();
+        try {
+            threadB.join();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        ArrayList<String> data = threadA.getData();
+        // total must be 20 (10 from A, 10 from B)
+        assertEquals(20, data.size());
+
+        // first 10 must be A's entries, next 10 must be B's entries
+        for (int i = 0; i < 10; i++) {
+            assertTrue("Index " + i + " should be from A3", data.get(i).startsWith("A3 "));
+        }
+        for (int i = 10; i < 20; i++) {
+            assertTrue("Index " + i + " should be from B3", data.get(i).startsWith("B3 "));
+        }
     }
 }
